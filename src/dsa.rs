b499ee7a0952273aa38f34932a3c2021012fef8d
ZitaRR <haeggqvist1998@hotmail.com>
2024-08-03 19:17:16 +0200

    some searching

--- src/dsa.rs
+++ src/dsa.rs
@@ -14,6 +14,13 @@ pub mod fibonacci{
 }
 
 pub mod arrays{
+    use std::cmp::Ordering;
+
+    pub struct SearchData{
+        pub index: isize,
+        pub operations: usize
+    }
+
     pub fn bubble_sort(list: &mut Vec<usize>){
         for i in 0..list.len(){
             for j in 0..list.len() - 1{
@@ -37,6 +44,39 @@ pub mod arrays{
             list.remove(index);
             list.insert(i, temp);
             print_array(list);
+        }
+    }
+
+    pub fn linear_search(list: &mut Vec<usize>, target: usize) -> SearchData{
+        for (i, value) in list.iter().enumerate(){
+            if *value == target{
+                return SearchData{
+                    index: i as isize,
+                    operations: i
+                }
+            }
+        }
+        return SearchData{
+            index: -1,
+            operations: list.len()
+        }
+    }
+
+    pub fn binary_search(list: &mut Vec<usize>, target: usize) -> SearchData{
+        return search(list, target, 0, list.len(), SearchData{index: 0, operations: 0});
+
+        fn search(list: &mut Vec<usize>, target: usize, min: usize, max: usize, data: SearchData) -> SearchData{
+            let mid = (min + max) / 2;
+            println!("Interval: {0}-{1}, Mid: {2}, Value: {3}, Target: {4}", min, max, mid, list[mid], target);
+            let data = SearchData{
+                index: mid as isize,
+                operations: data.operations + 1
+            };
+            return match list[mid].cmp(&target){
+                Ordering::Equal => data,
+                Ordering::Less => search(list, target, mid, max, data),
+                Ordering::Greater => search(list, target, min, mid, data)
+            }
         }
     }
 
